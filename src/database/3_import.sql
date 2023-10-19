e95aace9572d2e8f703345751c6426df10fe65b8
Matis Leima <matisleima@gmail.com>
2023-10-19 04:42:25 +0300

    Import SQL added

---
+++ src/database/3_import.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Väike kukeseenekoht minu talu metsas', 'A', 27.31579303690524, 57.62363339193397);
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Suur puravikukoht minu naabri metsas', 'A', 27.32210636037053, 57.625069609270305);
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Lottemaa seened', 'A', 24.58852291056246, 58.30227376563716);
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Vilsandi Vesiloo vöödikud', 'A', 21.82992935078801, 58.399980168460345);
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Tuumaseened', 'A', 25.841522184200585, 59.37239020676807);
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Gribõ', 'A', 28.219223006162796, 59.32968644439326);
+INSERT INTO public.location (id, description, status, latitude, longitude) VALUES (default, 'Lennukas seenekoht', 'A', 24.843902583816092, 59.413382377641234);
